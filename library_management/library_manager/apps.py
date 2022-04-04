a18890849c658ff7e7459385acca6623685b1e04
ankitj <ankit@fossee.in>
2022-04-04 12:55:15 +0530

    Add project skeleton

---
+++ library_management/library_manager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LibraryManagerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'library_manager'
---
+++ library_management/library_manager/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255)
+    isbn_num = models.CharField(max_length=150)
+    genre = models.CharField(max_length=15)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class BookStatus(models.Model):
+    STATUSES = (
+        ("Issued", "issued"),
+        ("Available", "available"),
+        ("Unavailable", "unavailable")
+    )
+    status = models.CharField(max_length=100, choices=STATUSES)
+    member = models.ForeignKey("Member", on_delete=models.CASCADE, related_name="user_status")
+    book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name="book_status")
